b58a3290cd34839e502c703bbbdb756c57c0f323
조은현-개인 <kgshdgeh@gmail.com>
2024-07-20 16:25:05 +0900

    chore: jotai, jotai-tanstack-query 설치

---
+++ src/app/providers.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Provider as JotaiProvider } from 'jotai'
+import { ReactNode, useState } from 'react'
+
+export function Providers({ children }: { children: ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient())
+
+  return (
+    <JotaiProvider>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </JotaiProvider>
+  )
+}
