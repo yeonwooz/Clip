40d589510599f1b02137daf81663b3ccd04c4320
Suyeon Woo <woosean999@gmail.com>
2024-07-27 01:45:17 +0900

    feat(Schedule): 드래그앤 드롭 개선

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -22,23 +22,23 @@ const SchedulePage: React.FC = () => {
 
         const { source, destination } = result;
 
-        // 같은 리스트 안에서의 이동
-        if (source.droppableId === destination.droppableId) {
-            const items = Array.from(data);
-            const [movedItem] = items[source.index].items.splice(source.index, 1);
-            items[destination.index].items.splice(destination.index, 0, movedItem);
-            setData(items);
-        } else {
-            // 다른 리스트로의 이동
-            const sourceItems = Array.from(data[source.droppableId].items);
-            const destinationItems = Array.from(data[destination.droppableId].items);
-            const [movedItem] = sourceItems.splice(source.index, 1);
-            destinationItems.splice(destination.index, 0, movedItem);
-            const newData = Array.from(data);
-            newData[source.droppableId].items = sourceItems;
-            newData[destination.droppableId].items = destinationItems;
-            setData(newData);
-        }
+        const sourceDayIndex = parseInt(source.droppableId.split('-')[0]);
+        const destDayIndex = parseInt(destination.droppableId.split('-')[0]);
+
+        const sourceIndex = source.index;
+        const destIndex = destination.index;
+
+        const sourceDay = data[sourceDayIndex];
+        const destDay = data[destDayIndex];
+
+        const [movedItem] = sourceDay.items.splice(sourceIndex, 1);
+        destDay.items.splice(destIndex, 0, movedItem);
+
+        const newData = Array.from(data);
+        newData[sourceDayIndex] = sourceDay;
+        newData[destDayIndex] = destDay;
+
+        setData(newData);
     };
 
     const formatTime = (time: string) => {
@@ -73,86 +73,93 @@ const SchedulePage: React.FC = () => {
         return timeSlots;
     };
 
-    const calculatePosition = (startTime, endTime) => {
-        const start = parseInt(startTime.slice(0, 2)) + parseInt(startTime.slice(2)) / 60;
-        const end = parseInt(endTime.slice(0, 2)) + parseInt(endTime.slice(2)) / 60;
-        return {
-            top: `${start * 60}px`,
-            height: `${(end - start) * 60}px`,
-        };
+    const addDummyItems = (daySchedule) => {
+        const timeSlots = getTimeSlots();
+        const existingTimes = daySchedule.items.map((item) => item.startTime.slice(0, 2) + ':00');
+        const dummyItems = timeSlots
+            .filter((timeSlot) => !existingTimes.includes(timeSlot))
+            .map((timeSlot, index) => ({
+                title: `빈 시간 ${index}`,
+                startTime: timeSlot.replace(':', '') + '00',
+                endTime: timeSlot.replace(':', '') + '59',
+                address: '',
+                isDummy: true, // 더미 아이템임을 표시
+            }));
+        return { ...daySchedule, items: [...daySchedule.items, ...dummyItems] };
     };
+
+    const dataWithDummyItems = data.map((daySchedule) => addDummyItems(daySchedule));
 
     return (
         <div className={styles.container}>
             <div className={styles.contents}>
                 <h1>부산여행</h1>
-                <div className={styles.scheduleGridContainer}>
-                    <div className={styles.timeLabels}>
-                        {getTimeSlots().map((timeSlot, idx) => (
-                            <div className={styles.timeLabel} key={idx}>
-                                {timeSlot}
-                            </div>
-                        ))}
-                    </div>
-                    <DragDropContext onDragEnd={onDragEnd}>
-                        <div className={styles.scheduleGrid}>
-                            {data.map((daySchedule, index) => (
-                                <Droppable droppableId={`${index}`} key={index} direction='horizontal'>
-                                    {(provided) => (
-                                        <div
-                                            className={styles.scheduleDay}
-                                            {...provided.droppableProps}
-                                            ref={provided.innerRef}
-                                        >
-                                            <div className={styles.dateTextContainer}>
-                                                <div className={styles.dateText}>{formatDate(daySchedule.date)}</div>
-                                            </div>
-                                            {getTimeSlots().map((timeSlot, timeIdx) => (
-                                                <div className={styles.timeSlotContainer} key={timeIdx}>
-                                                    <div className={styles.itemsContainer}>
-                                                        {daySchedule.items
-                                                            .filter((item) =>
-                                                                item.startTime.startsWith(timeSlot.slice(0, 2)),
-                                                            )
-                                                            .map((item, itemIdx) => (
-                                                                <Draggable
-                                                                    key={item.title}
-                                                                    draggableId={item.title}
-                                                                    index={itemIdx}
-                                                                >
-                                                                    {(provided) => (
-                                                                        <div
-                                                                            className={styles.scheduleItem}
-                                                                            ref={provided.innerRef}
-                                                                            {...provided.draggableProps}
-                                                                            {...provided.dragHandleProps}
-                                                                            style={{
-                                                                                ...provided.draggableProps.style,
-                                                                            }}
-                                                                        >
-                                                                            <p>
-                                                                                <strong>{item.title}</strong>
-                                                                            </p>
-                                                                            <p>
-                                                                                시간: {formatTime(item.startTime)} -{' '}
-                                                                                {formatTime(item.endTime)}
-                                                                            </p>
-                                                                            <p>주소: {item.address}</p>
-                                                                        </div>
-                                                                    )}
-                                                                </Draggable>
-                                                            ))}
-                                                    </div>
-                                                </div>
-                                            ))}
-                                            {provided.placeholder}
-                                        </div>
-                                    )}
-                                </Droppable>
+                {loading && <p>로딩 중...</p>}
+                {error && <p>에러 발생: {error?.message}</p>}
+                {!loading && !error && (
+                    <div className={styles.scheduleGridContainer}>
+                        <div className={styles.timeLabels}>
+                            {getTimeSlots().map((timeSlot, idx) => (
+                                <div className={styles.timeLabel} key={idx}>
+                                    {timeSlot}
+                                </div>
                             ))}
                         </div>
-                    </DragDropContext>
-                </div>
+                        <DragDropContext onDragEnd={onDragEnd}>
+                            <div className={styles.scheduleGrid}>
+                                {dataWithDummyItems.map((daySchedule, dayIndex) => (
+                                    <Droppable droppableId={`${dayIndex}`} key={dayIndex} type='ITEM'>
+                                        {(provided) => (
+                                            <div
+                                                className={styles.scheduleDay}
+                                                {...provided.droppableProps}
+                                                ref={provided.innerRef}
+                                            >
+                                                <div className={styles.dateTextContainer}>
+                                                    <div className={styles.dateText}>
+                                                        {formatDate(daySchedule.date)}
+                                                    </div>
+                                                </div>
+                                                {daySchedule.items.map((item, itemIdx) => (
+                                                    <Draggable
+                                                        key={`${dayIndex}-${item.title}-${item.startTime}`}
+                                                        draggableId={`${dayIndex}-${item.title}-${item.startTime}`}
+                                                        index={itemIdx}
+                                                    >
+                                                        {(provided) => (
+                                                            <div
+                                                                className={`${styles.scheduleItem} ${
+                                                                    item.isDummy ? styles.hiddenItem : ''
+                                                                }`}
+                                                                ref={provided.innerRef}
+                                                                {...provided.draggableProps}
+                                                                {...provided.dragHandleProps}
+                                                            >
+                                                                {!item.isDummy && (
+                                                                    <>
+                                                                        <p>
+                                                                            <strong>{item.title}</strong>
+                                                                        </p>
+                                                                        <p>
+                                                                            시간: {formatTime(item.startTime)} -{' '}
+                                                                            {formatTime(item.endTime)}
+                                                                        </p>
+                                                                        {item.address && <p>주소: {item.address}</p>}
+                                                                    </>
+                                                                )}
+                                                            </div>
+                                                        )}
+                                                    </Draggable>
+                                                ))}
+                                                {provided.placeholder}
+                                            </div>
+                                        )}
+                                    </Droppable>
+                                ))}
+                            </div>
+                        </DragDropContext>
+                    </div>
+                )}
             </div>
             <div className={styles.buttonGroup}>
                 <Button type='cancel' onClick={() => console.log('취소 버튼 클릭됨')}>
