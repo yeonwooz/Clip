5ceb2aca2e1237a3509b47aca344855ba5bee54b
Suyeon Woo <woosean999@gmail.com>
2024-07-27 17:35:58 +0900

    feat: rewrites 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+    reactStrictMode: true,
+    async rewrites() {
+        return [
+            {
+                source: '/schedule',
+                destination: 'http://118.67.130.17:8080/schedule',
+            },
+        ];
+    },
+};
 
 export default nextConfig;
